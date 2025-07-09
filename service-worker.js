2322a1492289c0597fb4475d5060984337975c99
ursuswork <ursuswork84@gmail.com>
2025-07-09 16:41:36 -0600

    agregar iconos nuevos

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
-self.addEventListener("install", function (e) {
-  console.log("Service Worker instalado");
+self.addEventListener('install', function(event) {
+  console.log('✅ Service Worker instalado');
 });
 
-self.addEventListener("fetch", function (e) {
-  // Se puede personalizar para cache offline
+self.addEventListener('fetch', function(event) {
+  // No hace nada especial todavía, solo deja pasar
 });
